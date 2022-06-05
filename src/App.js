da42713f78bc4cd520a8218ac025134429b5d7ec
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-06-05 18:22:55 -0500

    movement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 //npm run deploy
 import './App.css';
+import {useState, useEffect} from "react";
+
+
+
+
 
 function App() {
+
+  useEffect(() => {
+    document.addEventListener('keydown', detectKeyDown, true);
+
+  },[])
+
+  const detectKeyDown = (e) => {
+    console.log("clicked key",e.key)
+    let player = document.querySelector('.player');
+
+    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
+    let playerDown = parseInt(window.getComputedStyle(player).getPropertyValue("bottom"))
+    let playerLeft = parseInt(window.getComputedStyle(player).getPropertyValue("left"))
+
+
+    if (e.key === " "){
+      console.log("Key Clicked: Spacebar")
+    }
+
+    switch (e.key){
+      case 'ArrowRight': player.style.left = (playerLeft+5) + "px";
+      console.log(playerLeft)
+      break;
+      
+      case 'ArrowLeft': player.style.left = (playerLeft-5) + "px";
+      console.log(playerLeft)
+      break;
+
+      case 'ArrowUp': player.style.top = (playerTop-5) + "px";
+      console.log(playerTop)
+      break;
+
+      case 'ArrowDown': player.style.top = (playerTop+5) + "px";
+      console.log(playerTop)
+      break;
+  }
+
+
+  }
+
   return (
     <div className="App">
-      <a>HIIII</a>
-      <p1>test again</p1>
+      <div className='container'>
+        <div className="player"></div>
+      </div>
+      
     </div>
   );
 }
