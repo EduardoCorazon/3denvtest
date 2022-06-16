b59f1b8c2390727f73ae5356d478c4c1c45ada20
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-06-16 16:28:09 -0500

    new ideas

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 //npm run deploy
-import './App.css';
 import React from 'react';
-import { useRef, useState } from 'react'
-import ReactDOM from 'react-dom'
-import { Canvas, useFrame } from '@react-three/fiber'
-
-import { OrbitControls, Stars } from "drei";
-import { Physics, usePlane, useBox } from "use-cannon";
-
-
-
-function Box() {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
-  return (
-    <mesh
-      onClick={() => {
-        api.velocity.set(0, 2, 0);
-      }}
-      ref={ref}
-      position={[0, 2, 0]}
-    >
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
-    </mesh>
-  );
-}
-
-function Plane() {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-  }));
-  return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
-    </mesh>
-  );
-}
+import { Canvas, Vector3} from '@react-three/fiber'
+import { Sky} from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
 
 
 
 function App() {
   return (
     <Canvas>
-      <OrbitControls />
-      <Stars />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Physics>
-        <Box />
-        <Plane />
-      </Physics>
+        <Sky sunPosition={new Vector3(100,10,100)}/>
+        <ambientLight intensity={0.3}/>
+        <pointLight castShadow intensity={0.8} position={[100, 100, 100]}/>
+        
     </Canvas>
   );
 }
