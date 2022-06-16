1035bb9d48acffcb0e1b83485bf27a06d508542d
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-06-16 16:28:38 -0500

    new ideas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //npm run deploy
 import React from 'react';
-import { Canvas, Vector3} from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { Sky} from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 
@@ -12,7 +12,7 @@ function App() {
         <Sky sunPosition={new Vector3(100,10,100)}/>
         <ambientLight intensity={0.3}/>
         <pointLight castShadow intensity={0.8} position={[100, 100, 100]}/>
-        
+
     </Canvas>
   );
 }
