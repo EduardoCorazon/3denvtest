adc365d1a9d0808b27440424fddc51e4c7be81ed
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-06-16 19:15:56 -0500

    added mouse view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 //npm run deploy
 
 //Imports
-import React, { useRef } from 'react';
+//Basic things
+import React, { Suspense, useRef } from 'react';
 import './App.css'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { Sky } from "@react-three/drei";
-import { Physics } from "@react-three/cannon";
 import * as THREE from "three"
-import { Camera } from './Assets/3d-main/Camera';
+//React 3 fiber
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { PointerLockControls, Sky, Stars } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+//Asset Files
 
+
+
+//EXPERIMENT
 //Create an simple box object that rotates
 const Box = ({position}) => {
     const mesh = useRef(null)
@@ -23,12 +28,22 @@ const Box = ({position}) => {
 
 
 
+
+
 function App() {
   return (
     <Canvas colorManagement>
+        <PointerLockControls/>
+        
+        
+        
         <Sky sunPosition={new THREE.Vector3(100,10,100)}/>
+        <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
         <ambientLight intensity={0.3}/>
         <pointLight castShadow intensity={0.8} position={[100, 100, 100]}/>
+
+
+
 
         <Box position={[-2,1,0]}/>
         <Box position={[0,2,0]}/>
